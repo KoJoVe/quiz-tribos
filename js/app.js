632b8aeb4d47b85d391d88acd9724e427796f7f2
gberger42 <guilherme.berger@gmail.com>
2013-05-28 23:09:15 -0300

    votos e % votos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
 function QuizCtrl($scope){
 
   $scope.perguntaAtual = 0
-  $scope.respostas = []
+  $scope.triboVotos = [] //atualizada por updateTriboVotos
+  $scope.triboPorc = [] //atualizada por updatetriboPorc
 
   $scope.atualizaOpcao = function(i){
-    $scope.quiz[$scope.perguntaAtual].selecionada = i
+    $scope.perguntas[$scope.perguntaAtual].selecionada = i
   }
 
   $scope.proximaPergunta = function(){
-    alert($scope.quiz[$scope.perguntaAtual].selecionada)
-    $scope.respostas[$scope.perguntaAtual] = $scope.quiz[$scope.perguntaAtual].selecionada
-    $scope.perguntaAtual += 1
+    if($scope.perguntaAtual == $scope.perguntas.length - 1){
+      $scope.fim()
+    } else {
+      $scope.perguntaAtual += 1
+    }
+  }
+
+  $scope.updateTriboVotos = function() {
+    //inicializa array de votos
+    $scope.triboVotos = []
+    var length = $scope.tribos.length
+    for(var i = 0; i<length; i++){
+      $scope.triboVotos[i] = 0
+    }
+
+    //adiciona votos as respostas
+    $scope.perguntas.forEach(function(pergunta) {
+      var selecionada = pergunta.selecionada
+      var valores = pergunta.respostas[selecionada].valores
+      for(var i = 0; i<length; i++){
+        $scope.triboVotos[i] += valores[i]
+      }
+    })
+  }
+
+  $scope.updateTriboPorc = function() {
+    $scope.triboPorc = []
+    var length = $scope.triboVotos.length
+    var soma = eval($scope.triboVotos.join('+'))
+
+    if(soma == 0)
+      return;
+
+    for(var i = 0; i<length; i++){
+      $scope.triboPorc[i] = $scope.triboVotos[i]*100/soma
+    }
+  }
+
+  $scope.fim = function() {
+    $scope.updateTriboVotos()
+    $scope.updateTriboPorc()
+
+    alert(JSON.stringify($scope.triboPorc))
   }
 
   $scope.tribos = [
@@ -25,17 +66,17 @@ function QuizCtrl($scope){
     {nome: "skatista"},
   ]
 
-  $scope.quiz = [
+  $scope.perguntas = [
     {pergunta: 'Minha prioridade na hora de gastar o meu dinheiro é com:', respostas: [   
-      {resposta: 'roupas/meu carro,  aparência é o cartão de visitas.', valores: [0,2,0,0,0,0,0,0,0]},
-      {resposta: 'jogos, claro!', valores: [0,0,0,0,2,0,0,0,0]},
-      {resposta: 'suplemento, não adianta só se matar de malhar.', valores: [0,0,0,0,0,0,0,2,0]},
-      {resposta: 'shows de rock. Nunca perderia a oportunidade de ver uma  lenda ao vivo.', valores: [0,0,0,0,0,2,0,0,0]},
-      {resposta: 'parafina. Impossível é ficar sem surfar por um dia.', valores: [1,0,0,0,0,0,0,0,0]},
-      {resposta: 'equipamentos para o meu skate estar sempre preparado para as manobras.', valores: [0,0,0,0,0,0,0,0,2]},
-      {resposta: 'mostra de filmes. Não é fácil encontrar filmes de qualidade nos cinemas.', valores: [0,0,0,2,0,0,0,0,0]},
+      {resposta: 'Roupas/meu carro,  aparência é o cartão de visitas.', valores: [0,2,0,0,0,0,0,0,0]},
+      {resposta: 'Jogos, claro!', valores: [0,0,0,0,2,0,0,0,0]},
+      {resposta: 'Suplemento, não adianta só se matar de malhar.', valores: [0,0,0,0,0,0,0,2,0]},
+      {resposta: 'Shows de rock. Nunca perderia a oportunidade de ver uma  lenda ao vivo.', valores: [0,0,0,0,0,2,0,0,0]},
+      {resposta: 'Parafina. Impossível é ficar sem surfar por um dia.', valores: [1,0,0,0,0,0,0,0,0]},
+      {resposta: 'Equipamentos para o meu skate estar sempre preparado para as manobras.', valores: [0,0,0,0,0,0,0,0,2]},
+      {resposta: 'Mostra de filmes. Não é fácil encontrar filmes de qualidade nos cinemas.', valores: [0,0,0,2,0,0,0,0,0]},
       {resposta: 'Não gasto meu dinheiro, acredito em um mundo sustentável.', valores: [0,0,2,0,0,0,0,0,0]},
-      {resposta: 'com baile funk. Se joga!', valores: [0,0,0,0,0,0,2,0,0]}
+      {resposta: 'Com baile funk. Se joga!', valores: [0,0,0,0,0,0,2,0,0]}
     ]},
     {pergunta: 'O que mais me irrita é::', respostas: [   
       {resposta: 'Quando a compra não é autorizada.', valores: [0,1,0,0,0,0,0,0,0]},
@@ -49,73 +90,73 @@ function QuizCtrl($scope){
       {resposta: 'O fim da furacão.', valores: [0,0,0,0,0,0,1,0,0]}
     ]},
     {pergunta: 'Acordar cedo pra mim é:', respostas: [    
-      {resposta: 'moleza, eu curto a manhã', valores: [1,0,1,0,0,0,0,1,0]},
-      {resposta: 'impossível, eu durmo muito tarde', valores: [0,1,0,1,1,1,0,0,0]},
-      {resposta: 'aceitável, dependendo do que vou fazer', valores: [0,0,0,0,0,0,1,0,1]}
+      {resposta: 'Moleza, eu curto a manhã', valores: [1,0,1,0,0,0,0,1,0]},
+      {resposta: 'Impossível, eu durmo muito tarde.', valores: [0,1,0,1,1,1,0,0,0]},
+      {resposta: 'Aceitável, dependendo do que vou fazer.', valores: [0,0,0,0,0,0,1,0,1]}
     ]},
     {pergunta: 'Celular pra mim serve para:', respostas: [    
-      {resposta: 'conversar com a galera', valores: [0,1,0,0,0,0,0,0,0]},
-      {resposta: 'tirar foto', valores: [0,0,0,1,0,0,0,1,0]},
-      {resposta: 'só ligar e olhe lá', valores: [1,0,1,0,0,0,0,0,0]},
-      {resposta: 'ouvir música', valores: [0,0,0,0,0,1,1,0,1]},
-      {resposta: 'testando novos aplicativos', valores: [0,0,0,0,1,0,0,0,0]}
+      {resposta: 'Conversar com a galera.', valores: [0,1,0,0,0,0,0,0,0]},
+      {resposta: 'Tirar foto.', valores: [0,0,0,1,0,0,0,1,0]},
+      {resposta: 'Só ligar e olhe lá.', valores: [1,0,1,0,0,0,0,0,0]},
+      {resposta: 'Ouvir música.', valores: [0,0,0,0,0,1,1,0,1]},
+      {resposta: 'Testando novos aplicativos.', valores: [0,0,0,0,1,0,0,0,0]}
     ]},
     {pergunta: 'Uso muito a expressão:', respostas: [   
-      {resposta: 'relaxa ai', valores: [1,0,0,0,0,0,0,0,1]},
-      {resposta: 'coé', valores: [0,0,0,0,0,0,1,1,0]},
-      {resposta: 'como você nunca ouviu falar de ...', valores: [0,1,0,1,0,1,0,0,0]},
+      {resposta: 'Relaxa ai.', valores: [1,0,0,0,0,0,0,0,1]},
+      {resposta: 'Coé', valores: [0,0,0,0,0,0,1,1,0]},
+      {resposta: 'Como você nunca ouviu falar de ...', valores: [0,1,0,1,0,1,0,0,0]},
       {resposta: 'LOL', valores: [0,0,0,0,1,0,0,0,0]},
-      {resposta: 'energia muito boa', valores: [0,0,1,0,0,0,0,0,0]}
+      {resposta: 'Energia muito boa.', valores: [0,0,1,0,0,0,0,0,0]}
     ]},
     {pergunta: 'Meu sapato ideal é:', respostas: [    
-      {resposta: 'all-star preto', valores: [0,0,0,0,1,1,0,0,0]},
-      {resposta: 'descalço/ chinelo', valores: [1,0,1,0,0,0,0,0,0]},
-      {resposta: 'salto/topsider', valores: [0,1,0,0,0,0,0,0,0]},
-      {resposta: 'tenis de cano alto', valores: [0,0,0,1,0,0,0,0,1]},
-      {resposta: 'tenis/salto plataforma', valores: [0,0,0,0,0,0,1,1,0]}
+      {resposta: 'All-star preto.', valores: [0,0,0,0,1,1,0,0,0]},
+      {resposta: 'Descalço ou de chinelo.', valores: [1,0,1,0,0,0,0,0,0]},
+      {resposta: 'Salto ou topsider.', valores: [0,1,0,0,0,0,0,0,0]},
+      {resposta: 'Tênis de cano alto.', valores: [0,0,0,1,0,0,0,0,1]},
+      {resposta: 'Tênis ou salto plataforma.', valores: [0,0,0,0,0,0,1,1,0]}
     ]},
     {pergunta: 'Gosto de comer:', respostas: [    
-      {resposta: 'japonês', valores: [0,1,0,0,0,0,0,0,0]},
-      {resposta: 'saladinha', valores: [0,0,0,0,0,0,0,1,0]},
-      {resposta: 'sanduba natural', valores: [1,0,1,0,0,0,0,0,0]},
-      {resposta: 'fast food', valores: [0,0,0,0,1,1,0,0,1]},
-      {resposta: 'fondue', valores: [0,0,0,1,0,0,0,0,0]},
-      {resposta: 'churrasco', valores: [0,0,0,0,0,0,1,0,0]}
+      {resposta: 'Japonês.', valores: [0,1,0,0,0,0,0,0,0]},
+      {resposta: 'Saladinha.', valores: [0,0,0,0,0,0,0,1,0]},
+      {resposta: 'Sanduba natural.', valores: [1,0,1,0,0,0,0,0,0]},
+      {resposta: 'Fast food.', valores: [0,0,0,0,1,1,0,0,1]},
+      {resposta: 'Fondue.', valores: [0,0,0,1,0,0,0,0,0]},
+      {resposta: 'Churrasco.', valores: [0,0,0,0,0,0,1,0,0]}
     ]},
     {pergunta: 'Aproveito os feriadões para:', respostas: [   
-      {resposta: 'fazer atividades ao ar livre', valores: [1,0,1,0,0,0,0,0,1]},
-      {resposta: 'ir para um festival de rock', valores: [0,0,0,0,1,1,0,0,0]},
-      {resposta: 'ler um bom livro.', valores: [0,0,0,1,0,0,0,0,0]},
-      {resposta: 'ir pra night', valores: [0,1,0,0,0,0,1,1,0]}
+      {resposta: 'Fazer atividades ao ar livre.', valores: [1,0,1,0,0,0,0,0,1]},
+      {resposta: 'Ir para um festival de rock.', valores: [0,0,0,0,1,1,0,0,0]},
+      {resposta: 'Ler um bom livro.', valores: [0,0,0,1,0,0,0,0,0]},
+      {resposta: 'Ir pra night.', valores: [0,1,0,0,0,0,1,1,0]}
     ]},
     {pergunta: 'Quando não estou dormindo eu estou:', respostas: [    
-      {resposta: 'no computador', valores: [0,0,0,1,1,0,0,0,0]},
-      {resposta: 'me exercitanto', valores: [1,0,0,0,0,0,0,1,0]},
-      {resposta: 'na rua', valores: [0,0,0,0,0,0,1,0,1]},
-      {resposta: 'ouvindo música', valores: [0,0,0,0,0,1,0,0,0]},
-      {resposta: 'no meio do mato', valores: [0,0,1,0,0,0,0,0,0]},
-      {resposta: 'fazendo compras', valores: [0,1,0,0,0,0,0,0,0]}
+      {resposta: 'No computador.', valores: [0,0,0,1,1,0,0,0,0]},
+      {resposta: 'Me exercitanto.', valores: [1,0,0,0,0,0,0,1,0]},
+      {resposta: 'Na rua.', valores: [0,0,0,0,0,0,1,0,1]},
+      {resposta: 'Ouvindo música.', valores: [0,0,0,0,0,1,0,0,0]},
+      {resposta: 'No meio do mato.', valores: [0,0,1,0,0,0,0,0,0]},
+      {resposta: 'Fazendo compras.', valores: [0,1,0,0,0,0,0,0,0]}
     ]},
     {pergunta: 'Que estilo não falta na sua playlist', respostas: [   
-      {resposta: 'Rap', valores: [0,0,0,0,0,0,0,0,1]},
-      {resposta: 'Rock', valores: [0,0,0,0,1,1,0,0,0]},
-      {resposta: 'Funk', valores: [0,0,0,0,0,0,1,0,0]},
-      {resposta: 'Eletrônica / Hip Hop', valores: [0,1,0,0,0,0,0,1,0]},
-      {resposta: 'Jazz', valores: [0,0,0,1,0,0,0,0,0]},
-      {resposta: 'Reggae', valores: [1,0,1,0,0,0,0,0,0]}
+      {resposta: 'Rap.', valores: [0,0,0,0,0,0,0,0,1]},
+      {resposta: 'Rock.', valores: [0,0,0,0,1,1,0,0,0]},
+      {resposta: 'Funk.', valores: [0,0,0,0,0,0,1,0,0]},
+      {resposta: 'Eletrônica / Hip Hop.', valores: [0,1,0,0,0,0,0,1,0]},
+      {resposta: 'Jazz.', valores: [0,0,0,1,0,0,0,0,0]},
+      {resposta: 'Reggae.', valores: [1,0,1,0,0,0,0,0,0]}
     ]},
     {pergunta: 'Festa boa pra mim tem:', respostas: [   
-      {resposta: 'Gente bonita', valores: [0,1,0,0,0,0,0,1,0]},
-      {resposta: 'Open bar', valores: [0,0,0,0,0,0,1,0,0]},
-      {resposta: 'Energia boa', valores: [1,0,1,0,0,0,0,0,1]},
-      {resposta: 'Música de qualidade', valores: [0,0,0,1,0,1,0,0,0]},
-      {resposta: 'O conforto da minha casa', valores: [0,0,0,0,1,0,0,0,0]}
+      {resposta: 'Gente bonita.', valores: [0,1,0,0,0,0,0,1,0]},
+      {resposta: 'Open bar.', valores: [0,0,0,0,0,0,1,0,0]},
+      {resposta: 'Energia boa.', valores: [1,0,1,0,0,0,0,0,1]},
+      {resposta: 'Música de qualidade.', valores: [0,0,0,1,0,1,0,0,0]},
+      {resposta: 'O conforto da minha casa.', valores: [0,0,0,0,1,0,0,0,0]}
     ]},
     {pergunta: 'Se o mundo acabasse hoje, eu:', respostas: [    
-      {resposta: 'Morreria lindo (a)', valores: [0,1,0,0,0,0,1,1,0]},
-      {resposta: 'Não pensei no assunto', valores: [1,0,0,0,0,0,0,0,1]},
-      {resposta: 'não seria nada. Nós somos apenas parte de um grande incerto', valores: [0,0,1,1,0,0,0,0,0]},
-      {resposta: 'seria guardado pra eternidade.', valores: [0,0,0,0,1,1,0,0,0]},
+      {resposta: 'Morreria lindo(a).', valores: [0,1,0,0,0,0,1,1,0]},
+      {resposta: 'Não pensei no assunto.', valores: [1,0,0,0,0,0,0,0,1]},
+      {resposta: 'Não seria nada. Nós somos apenas parte de um grande incerto.', valores: [0,0,1,1,0,0,0,0,0]},
+      {resposta: 'Seria guardado pra eternidade.', valores: [0,0,0,0,1,1,0,0,0]},
     ]}
   ]
 
